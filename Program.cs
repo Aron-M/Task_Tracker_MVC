ee2b527616df337b7a1eae4be795f32264fd0be1
Aron-M <aron.moelich88@gmail.com>
2024-04-07 12:48:34 +0100

    Updating Program.cs to include all APIServices

--- Program.cs
+++ Program.cs
@@ -3,13 +3,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Register the CategoryAPIService with HttpClient
+builder.Services.AddHttpClient<CategoryAPIService>(client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000/api/categories/");
+});
+
+// Register the TaskAPIService with HttpClient
+builder.Services.AddHttpClient<TaskAPIService>(client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000/api/tasks/");
+});
+
+// Register the UserAPIService with HttpClient
+builder.Services.AddHttpClient<UserAPIService>(client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000/api/users/");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
