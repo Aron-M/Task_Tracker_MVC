64ad8e6b5b90db0c4f15d19f0127cc7ecdc9ec5c
Aron-M <aron.moelich88@gmail.com>
2024-04-07 11:28:16 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TaskTrackerMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
